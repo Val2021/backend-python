edf701dce8d9cf6d4fbfe187f131b9bac46c2652
Val Araujo <val.araujo@businessplanner.com.br>
2022-01-15 14:20:55 -0300

    feat: creating UserRepository with insert_user and test it

--- src/infra/entities/users.py
+++ src/infra/entities/users.py
@@ -9,7 +9,7 @@ class Users(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False, unique=True)
+    name = Column(String, nullable=False)
     password = Column(String, nullable=False)
     id_pet = relationship("Pets")
 
